f8cd05952cf3a38e3b6439a6e342e7ce637e5d74
deivydasz <deivydas.ziziliauskas@gmail.com>
2021-05-11 11:54:14 +0300

    Add food class with specs

--- spec/foodie_spec.rb
+++ spec/foodie_spec.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
-RSpec.describe Foodie do
-  it "has a version number" do
-    expect(Foodie::VERSION).not_to be nil
+RSpec.describe Foodie::Food do
+  it "broccoli is gross" do
+    expect(Foodie::Food.portray("Broccoli")).to eql("Gross!")
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "anything else is delicious" do
+    expect(Foodie::Food.portray("Not Broccoli")).to eql("Delicious!")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'spec_helper'
 require "foodie"
 
 RSpec.configure do |config|
